961fe2ad978566b60d69c8fbe009aad0f48729a0
nsgxi43 <nishanthsg38@gmail.com>
2026-05-18 01:07:29 +0530

    refactor: clean repository structure and remove development artifacts

--- generate_hashes.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-async function generateHashes() {
-  const hash = await bcrypt.hash("password123", 10);
-  console.log(hash);
-}
-
-generateHashes();
--- test-updates.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const count = await prisma.quarterlyUpdate.count();
-  console.log("Total updates: " + count);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
