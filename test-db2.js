91fbe5105e9c7ecfd36adb884bb4f24ba9510062
nsgxi43 <nishanthsg38@gmail.com>
2026-05-17 19:05:46 +0530

    Clean debug scripts and prepare repository for demo

--- test-db2.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const gs = await prisma.goalSheet.findMany({ include: { employee: true } });
-  console.log(JSON.stringify(gs.map(g => ({ id: g.id, emp: g.employee.name, year: g.cycleYear })), null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
